76b324122cfec945d746943009fc7d7c7b4101a5
jack <1395093509@qq.com>
2018-06-12 09:53:05 +0800

    table示例

--- src/router/index.js
+++ src/router/index.js
@@ -72,73 +72,6 @@ export default new Router({
 });
 
 export const asyncRouterMap = [
-  // {
-  //   path: '/example',
-  //   component: Layout,
-  //   redirect: '/example/table/complex-table',
-  //   name: 'example',
-  //   meta: {
-  //     title: 'example',
-  //     icon: 'example',
-  //   },
-  //   children: [
-  //     {
-  //       path: '/example/table',
-  //       component: _import('example/table/index'),
-  //       redirect: '/example/table/complex-table',
-  //       name: 'Table',
-  //       meta: {
-  //         title: 'Table',
-  //         icon: 'table',
-  //       },
-  //       children: [
-  //         {
-  //           path: 'dynamic-table',
-  //           component: _import('example/table/dynamicTable/index'),
-  //           name: 'dynamicTable',
-  //           meta: { title: 'dynamicTable' },
-  //         },
-  //         {
-  //           path: 'drag-table',
-  //           component: _import('example/table/dragTable'),
-  //           name: 'dragTable',
-  //           meta: { title: 'dragTable' },
-  //         },
-  //         {
-  //           path: 'inline-edit-table',
-  //           component: _import('example/table/inlineEditTable'),
-  //           name: 'inlineEditTable',
-  //           meta: { title: 'inlineEditTable' },
-  //         },
-  //         {
-  //           path: 'tree-table',
-  //           component: _import('example/table/treeTable/treeTable'),
-  //           name: 'treeTableDemo',
-  //           meta: { title: 'treeTable' },
-  //         },
-  //         {
-  //           path: 'custom-tree-table',
-  //           component: _import('example/table/treeTable/customTreeTable'),
-  //           name: 'customTreeTableDemo',
-  //           meta: { title: 'customTreeTable' },
-  //         },
-  //         {
-  //           path: 'complex-table',
-  //           component: _import('example/table/complexTable'),
-  //           name: 'complexTable',
-  //           meta: { title: 'complexTable' },
-  //         },
-  //       ],
-  //     },
-  //     {
-  //       path: 'tab/index',
-  //       icon: 'tab',
-  //       component: _import('example/tab/index'),
-  //       name: 'tab',
-  //       meta: { title: 'tab' },
-  //     },
-  //   ],
-  // },
   {
     path: '/system',
     component: Layout,
@@ -255,7 +188,74 @@ export const asyncRouterMap = [
       // },
     ],
   },
-
+  {
+    path: '/example',
+    component: Layout,
+    redirect: 'noredirect',
+    // redirect: '/example/table/complex-table',
+    name: 'example',
+    meta: {
+      title: 'example',
+      icon: 'example',
+    },
+    children: [
+      {
+        path: '/example/table',
+        component: _import('example/table/index'),
+        redirect: '/example/table/complex-table',
+        name: 'Table',
+        meta: {
+          title: 'Table',
+          icon: 'table',
+        },
+        children: [
+          {
+            path: 'dynamic-table',
+            component: _import('example/table/dynamicTable/index'),
+            name: 'dynamicTable',
+            meta: { title: 'dynamicTable' },
+          },
+          {
+            path: 'drag-table',
+            component: _import('example/table/dragTable'),
+            name: 'dragTable',
+            meta: { title: 'dragTable' },
+          },
+          {
+            path: 'inline-edit-table',
+            component: _import('example/table/inlineEditTable'),
+            name: 'inlineEditTable',
+            meta: { title: 'inlineEditTable' },
+          },
+          {
+            path: 'tree-table',
+            component: _import('example/table/treeTable/treeTable'),
+            name: 'treeTableDemo',
+            meta: { title: 'treeTable' },
+          },
+          {
+            path: 'custom-tree-table',
+            component: _import('example/table/treeTable/customTreeTable'),
+            name: 'customTreeTableDemo',
+            meta: { title: 'customTreeTable' },
+          },
+          {
+            path: 'complex-table',
+            component: _import('example/table/complexTable'),
+            name: 'complexTable',
+            meta: { title: 'complexTable' },
+          },
+        ],
+      },
+      {
+        path: 'tab/index',
+        icon: 'tab',
+        component: _import('example/tab/index'),
+        name: 'tab',
+        meta: { title: 'tab' },
+      },
+    ],
+  },
   // {
   //   path: '/example',
   //   component: Layout,
