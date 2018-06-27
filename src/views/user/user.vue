0a07f8418f0ca987e1941ef359bb1401ea897ad7
jack <1395093509@qq.com>
2018-06-27 09:48:27 +0800

    更新demo

--- src/views/user/user.vue
+++ src/views/user/user.vue
@@ -1,24 +1,28 @@
 <template>
   <div class="app-container calendar-list-container">
     <div class="filter-container">
-      <el-input @keyup.enter.native="handleFilter" style="width: 200px;" class="filter-item" :placeholder="$t('table.title')" v-model="listQuery.title">
-      </el-input>
-      <!-- <el-select clearable style="width: 90px" class="filter-item" v-model="listQuery.importance" :placeholder="$t('table.importance')">
-        <el-option v-for="item in importanceOptions" :key="item" :label="item" :value="item">
-        </el-option>
-      </el-select> -->
-      <el-select clearable class="filter-item" style="width: 130px" v-model="listQuery.type" :placeholder="$t('table.type')">
-        <el-option v-for="item in calendarTypeOptions" :key="item.key" :label="item.display_name+'('+item.key+')'" :value="item.key">
-        </el-option>
-      </el-select>
-      <!-- <el-select @change='handleFilter' style="width: 140px" class="filter-item" v-model="listQuery.sort">
-        <el-option v-for="item in sortOptions" :key="item.key" :label="item.label" :value="item.key">
-        </el-option>
-      </el-select> -->
-      <el-button class="filter-item" type="primary" v-waves icon="el-icon-search" @click="handleFilter">{{$t('table.search')}}</el-button>
-      <el-button class="filter-item" style="margin-left: 10px;" @click="handleCreate" type="primary" icon="el-icon-edit">{{$t('table.add')}}</el-button>
-      <!-- <el-button class="filter-item" type="primary" :loading="downloadLoading" v-waves icon="el-icon-download" @click="handleDownload">{{$t('table.export')}}</el-button> -->
-      <!-- <el-checkbox class="filter-item" style='margin-left:15px;' @change='tableKey=tableKey+1' v-model="showReviewer">{{$t('table.reviewer')}}</el-checkbox> -->
+      <el-form
+      :inline="true"
+      :model="queryForm">
+        <el-input @keyup.enter.native="handleFilter" style="width: 200px;" class="filter-item" :placeholder="$t('table.title')" v-model="queryForm.title">
+        </el-input>
+        <!-- <el-select clearable style="width: 90px" class="filter-item" v-model="queryForm.importance" :placeholder="$t('table.importance')">
+          <el-option v-for="item in importanceOptions" :key="item" :label="item" :value="item">
+          </el-option>
+        </el-select> -->
+        <el-select clearable class="filter-item" style="width: 130px" v-model="queryForm.type" :placeholder="$t('table.type')">
+          <el-option v-for="item in calendarTypeOptions" :key="item.key" :label="item.display_name+'('+item.key+')'" :value="item.key">
+          </el-option>
+        </el-select>
+        <!-- <el-select @change='handleFilter' style="width: 140px" class="filter-item" v-model="queryForm.sort">
+          <el-option v-for="item in sortOptions" :key="item.key" :label="item.label" :value="item.key">
+          </el-option>
+        </el-select> -->
+        <el-button class="filter-item" type="primary" v-waves icon="el-icon-search" @click="handleFilter">{{$t('table.search')}}</el-button>
+        <el-button class="filter-item" style="margin-left: 10px;" @click="handleCreate" type="primary" icon="el-icon-edit">{{$t('table.add')}}</el-button>
+        <!-- <el-button class="filter-item" type="primary" :loading="downloadLoading" v-waves icon="el-icon-download" @click="handleDownload">{{$t('table.export')}}</el-button> -->
+        <!-- <el-checkbox class="filter-item" style='margin-left:15px;' @change='tableKey=tableKey+1' v-model="showReviewer">{{$t('table.reviewer')}}</el-checkbox> -->
+      </el-form>
     </div>
 
     <el-table :key='tableKey' :data="list" v-loading="listLoading" element-loading-text="给我一点时间" border fit highlight-current-row
@@ -79,7 +83,7 @@
     </el-table>
 
     <div class="pagination-container">
-      <el-pagination background @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="listQuery.page" :page-sizes="[10,20,30, 50]" :page-size="listQuery.limit" layout="total, sizes, prev, pager, next, jumper" :total="total">
+      <el-pagination background @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="queryForm.page" :page-sizes="[10, 20,30, 50]" :page-size="queryForm.limit" layout="total, sizes, prev, pager, next, jumper" :total="total">
       </el-pagination>
     </div>
 
@@ -160,7 +164,7 @@ export default {
       list: null,
       total: null,
       listLoading: true,
-      listQuery: {
+      queryForm: {
         page: 1,
         limit: 20,
         importance: undefined,
@@ -170,7 +174,10 @@ export default {
       },
       importanceOptions: [1, 2, 3],
       calendarTypeOptions,
-      sortOptions: [{ label: 'ID Ascending', key: '+id' }, { label: 'ID Descending', key: '-id' }],
+      sortOptions: [
+        { label: 'ID Ascending', key: '+id' },
+        { label: 'ID Descending', key: '-id' },
+      ],
       statusOptions: ['published', 'draft', 'deleted'],
       showReviewer: false,
       temp: {
@@ -208,6 +215,10 @@ export default {
     }
   },
   filters: {
+    sexFilter(value) {
+      const sexMap = ['未知', '男', '女'];
+      return sexMap[value];
+    },
     statusFilter(status) {
       const statusMap = {
         published: 'success',
@@ -226,22 +237,22 @@ export default {
   methods: {
     getList() {
       this.listLoading = true
-      fetchList(this.listQuery).then((res) => {
+      fetchList(this.queryForm).then((res) => {
         this.list = res.data.items
         this.total = res.data.total
         this.listLoading = false
       })
     },
     handleFilter() {
-      this.listQuery.page = 1
+      this.queryForm.page = 1
       this.getList()
     },
     handleSizeChange(val) {
-      this.listQuery.limit = val
+      this.queryForm.limit = val
       this.getList()
     },
     handleCurrentChange(val) {
-      this.listQuery.page = val
+      this.queryForm.page = val
       this.getList()
     },
     handleModifyStatus(row, status) {
